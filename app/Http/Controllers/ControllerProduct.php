5d87c61b9bb8d3647deee09662f978780a1938e8
Gribinenko <andrew.gribinenko@mainlinemenswear.co.uk>
2024-06-13 15:11:10 +0300

    autocomplete add basics

--- app/Http/Controllers/ControllerProduct.php
+++ app/Http/Controllers/ControllerProduct.php
@@ -12,11 +12,26 @@ class ControllerProduct extends Controller
     {
         $page = $request->input('page', 1);
         $pageSize = $request->input('pageSize', 10);
+        $filter = $request->input('filter', null);
+        $search = $request->input('search', '');
 
-        $list = Product::all()
-            ->forPage($page, $pageSize);
+        $query = Product::query();
 
-        return response()->json($list);
+        if (!empty($filter)) {
+            $query->where($filter, 'like', "%{$search}%");
+        }
+
+        $queryMeta = clone $query;
+
+        return response()
+            ->json([
+                'meta' => [
+                    'total' => $queryMeta->count(),
+                ],
+                'items' => $query->forPage($page, $pageSize)
+                    ->get()
+                    ->all(),
+            ]);
     }
 
     public function item(Request $request, $id)
